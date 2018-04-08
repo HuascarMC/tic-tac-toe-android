c5074ff206130f767a99f8f9fc11057b01d9c96c
Huascar <huascarmc@hotmail.com>
2018-04-08 19:25:31 +0100

    added humanplayer test get,set token and play

---
+++ app/src/test/java/com/example/huascar/tic_tac_toe/HumanPlayerTest.java
@@ -0,0 +1,41 @@
+package com.example.huascar.tic_tac_toe;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by huascar on 08/04/2018.
+ */
+
+public class HumanPlayerTest {
+
+    private HumanPlayer humanPlayer;
+    private Board board;
+    @Before
+    public void before() {
+        humanPlayer = new HumanPlayer("O");
+        board = new Board();
+    }
+
+    @Test
+    public void testHasToken() {
+        assertNotNull( humanPlayer.getToken() );
+    }
+
+    @Test
+    public void testPlayGridOne() {
+        humanPlayer.play(board, 0);
+        String[] grid = board.getGrid();
+        assertEquals( humanPlayer.getToken(), grid[0] );
+    }
+
+    @Test
+    public void testSetToken() {
+        humanPlayer.setToken("O");
+        assertEquals( humanPlayer.getToken(), "O");
+    }
+
+
+}
