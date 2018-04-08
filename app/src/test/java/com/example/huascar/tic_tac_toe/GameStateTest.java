110ba3468b03de0596f71d843e585799cddff026
Huascar <huascarmc@hotmail.com>
2018-04-08 17:51:34 +0100

    setup, added test finished with win

--- app/src/test/java/com/example/huascar/tic_tac_toe/GameStateTest.java
+++ app/src/test/java/com/example/huascar/tic_tac_toe/GameStateTest.java
@@ -1,30 +1,34 @@
-//package com.example.huascar.tic_tac_toe;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-///**
-// * Created by huascar on 08/04/2018.
-// */
-//
-//public class GameStateTest {
-//
-//    private GameState gameState;
-//    private Board board;
-//
-//    @Before
-//    public void before() {
-//        board = new Board();
-//        gameState = new GameState();
-//    }
-//
-//    @Test
-//    public void TestNotFinished() {
-//        assertEquals( gameState.finished(board), false );
-//    }
-//
-////    @Test
-////    public void testFinishedWithWin() {
-////        board.grid = ["O", "1", "X", "O", "X", "5", "O", "7", "8"];
-////    }
-//}
+package com.example.huascar.tic_tac_toe;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by huascar on 08/04/2018.
+ */
+
+public class GameStateTest {
+
+    private GameState gameState;
+    private Board board;
+
+    @Before
+    public void before() {
+        board = new Board();
+        gameState = new GameState();
+    }
+
+    @Test
+    public void TestNotFinished() {
+        assertEquals( gameState.finished(board), false );
+    }
+
+    @Test
+    public void testFinishedWithWin() {
+        String[] newGrid = new String[]{"O", "1", "X", "O", "X", "5", "O", "7", "8"};
+        board.setGrid(newGrid);
+        assertEquals( gameState.finished(board), true);
+    }
+}
