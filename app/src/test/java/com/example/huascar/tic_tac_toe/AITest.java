92b44786e134e60e01166910129b267003e91921
Huascar <huascarmc@hotmail.com>
2018-04-08 20:39:00 +0100

    added tests token, winner property AI

--- app/src/test/java/com/example/huascar/tic_tac_toe/AITest.java
+++ app/src/test/java/com/example/huascar/tic_tac_toe/AITest.java
@@ -1,8 +1,40 @@
 package com.example.huascar.tic_tac_toe;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
 /**
  * Created by huascar on 08/04/2018.
  */
 
 public class AITest {
+
+    private AI miniMax;
+    private Board board;
+
+    @Before
+    public void before() {
+        board = new Board();
+        miniMax = new Minimax("X");
+    }
+
+    @Test
+    public void testHasToken() {
+        assertEquals( mimiMax.getToken(), "X" );
+    }
+
+    @Test
+    public void testHasWinnerProperty() {
+        assertEquals( miniMax.getWinner(), null );
+    }
+
+    @Test
+    public void testSetsWinnerX() {
+        miniMax.setWinner("X")
+        assertEquals( miniMax.getWinner(), "X" );
+    }
+
+
 }
