3374ad68eab436827142ab0c085eecb68a36db36
Huascar <huascarmc@hotmail.com>
2018-04-10 21:56:00 +0100

    aded test has current player token

--- app/src/test/java/com/example/huascar/tic_tac_toe/HandleTurnsTest.java
+++ app/src/test/java/com/example/huascar/tic_tac_toe/HandleTurnsTest.java
@@ -1,6 +1,10 @@
 package com.example.huascar.tic_tac_toe;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static android.telecom.Call.Details.hasProperty;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by huascar on 10/04/2018.
@@ -11,5 +15,10 @@ public class HandleTurnsTest {
     @Before
     public void before() {
         handleTurns = new HandleTurns();
+    }
+
+    @Test
+    public void testHasCurrentPlayerTokenProperty() {
+        assertThat( handleTurns, hasProperty(currentPlayerToken) );
     }
 }
