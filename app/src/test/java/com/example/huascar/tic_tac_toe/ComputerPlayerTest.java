b9a57317af66349b0dc19edb689de72a720383eb
Huascar <huascarmc@hotmail.com>
2018-04-10 21:25:22 +0100

    computer player has standard tests + initializeAI test

---
+++ app/src/test/java/com/example/huascar/tic_tac_toe/ComputerPlayerTest.java
@@ -0,0 +1,66 @@
+package com.example.huascar.tic_tac_toe;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by huascar on 08/04/2018.
+ */
+
+public class ComputerPlayerTest {
+
+    private ComputerPlayer computerPlayer;
+    private Board board;
+
+    @Before
+    public void before() {
+        computerPlayer = new ComputerPlayer();
+        computerPlayer.setToken("O");
+        board = new Board();
+    }
+
+    @Test
+    public void testHasToken() {
+
+        assertNotNull( computerPlayer.getToken() );
+    }
+
+    @Test
+    public void testPlayGridOne() {
+        computerPlayer.play(board, 0);
+        String[] grid = board.getGrid();
+        assertEquals( computerPlayer.getToken(), grid[0] );
+    }
+
+    @Test
+    public void testSetTokenO() {
+        computerPlayer.setToken("O");
+        assertEquals( computerPlayer.getToken(), "O" );
+    }
+
+    @Test
+    public void testSetTokenX() {
+        computerPlayer.setToken("X");
+        assertEquals( computerPlayer.getToken(), "X" );
+    }
+
+    @Test
+    public void testAutoTokenX() {
+        computerPlayer.autoToken("X");
+        assertEquals( computerPlayer.getToken(), "O");
+    }
+
+    @Test
+    public void testAutoTokenO() {
+        computerPlayer.autoToken("O");
+        assertEquals( computerPlayer.getToken(), "X");
+    }
+
+    @Test
+    public void testInitializeAI() {
+        String token = computerPlayer.getToken();
+        assertNotNull( computerPlayer.AI );
+    }
+}
