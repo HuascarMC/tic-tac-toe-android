a05f6686b7632c307bc409b44bcd89b216abf92d
Huascar <huascarmc@hotmail.com>
2018-04-10 21:29:13 +0100

    created computer class extends from player, initialize AiI test failed

---
+++ app/src/main/java/com/example/huascar/tic_tac_toe/ComputerPlayer.java
@@ -0,0 +1,37 @@
+package com.example.huascar.tic_tac_toe;
+
+/**
+ * Created by huascar on 08/04/2018.
+ */
+
+public class ComputerPlayer extends Player {
+
+    private String token;
+    AI AI;
+
+    public ComputerPlayer() {
+        super();
+        AI AI = null;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void autoToken(String token) {
+        if(token == "X") {
+            this.setToken("O");
+        } else {
+            this.setToken("X");
+        }
+    }
+
+    public void play(Board board, int spot) {
+        board.setSpot(spot, token);
+    }
+
+}
