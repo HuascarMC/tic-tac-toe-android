8036d451b95c46c1fb60350226b030f2af58243c
Huascar <huascarmc@hotmail.com>
2018-04-11 16:17:46 +0100

    created test file fstup"

---
+++ app/src/test/java/com/example/huascar/tic_tac_toe/FirstPlayerSetupTest.java
@@ -0,0 +1,17 @@
+package com.example.huascar.tic_tac_toe;
+
+import com.example.huascar.tic_tac_toe.activities.FirstPlayerSetUp;
+
+import org.junit.Before;
+
+public class FirstPlayerSetupTest {
+
+    private FirstPlayerSetUp firstPlayerSetUp;
+
+    @Before
+    public void before() {
+        firstPlayerSetUp = new FirstPlayerSetUp();
+    }
+
+
+}
