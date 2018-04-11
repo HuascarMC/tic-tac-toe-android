8abec94af4592f90a93c7e1a05dbca170f2c5491
Huascar <huascarmc@hotmail.com>
2018-04-11 16:25:00 +0100

    added getter for token test passed

--- app/src/main/java/com/example/huascar/tic_tac_toe/activities/FirstPlayerSetUp.java
+++ app/src/main/java/com/example/huascar/tic_tac_toe/activities/FirstPlayerSetUp.java
@@ -7,9 +7,15 @@ import com.example.huascar.tic_tac_toe.R;
 
 public class FirstPlayerSetUp extends AppCompatActivity {
 
+    private String token;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_player_setup);
+    }
+
+    public String getToken() {
+        return token;
     }
 }
