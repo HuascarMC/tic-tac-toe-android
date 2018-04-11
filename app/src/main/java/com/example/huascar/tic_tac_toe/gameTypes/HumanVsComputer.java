2849f35cfbda0b0e2212f2981b161b407be3c84d
Huascar <huascarmc@hotmail.com>
2018-04-11 09:48:57 +0100

    created humanvscomputer gametype

--- app/src/main/java/com/example/huascar/tic_tac_toe/gameTypes/HumanVsComputer.java
+++ app/src/main/java/com/example/huascar/tic_tac_toe/gameTypes/HumanVsComputer.java
@@ -1,8 +1,25 @@
 package com.example.huascar.tic_tac_toe.gameTypes;
 
+import com.example.huascar.tic_tac_toe.Board;
+import com.example.huascar.tic_tac_toe.ComputerPlayer;
+import com.example.huascar.tic_tac_toe.GameState;
+import com.example.huascar.tic_tac_toe.HumanPlayer;
+
 /**
  * Created by huascar on 11/04/2018.
  */
 
 public class HumanVsComputer {
+
+    private Board board;
+    private GameState gameState;
+    private HumanPlayer humanPlayer;
+    private ComputerPlayer computerPlayer;
+
+    public HumanVsComputer() {
+        board = new Board();
+        gameState = new GameState();
+        computerPlayer = new ComputerPlayer();
+        humanPlayer = new HumanPlayer();
+    }
 }
