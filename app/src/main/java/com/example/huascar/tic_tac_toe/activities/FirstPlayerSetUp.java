356572a7f382ec1507c8052fdb1fc269c7f7afcd
Huascar <huascarmc@hotmail.com>
2018-04-11 11:20:48 +0100

    minor update first player setup

--- app/src/main/java/com/example/huascar/tic_tac_toe/activities/FirstPlayerSetUp.java
+++ app/src/main/java/com/example/huascar/tic_tac_toe/activities/FirstPlayerSetUp.java
@@ -10,6 +10,6 @@ public class FirstPlayerSetUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup);
+        setContentView(R.layout.activity_first_player_setup);
     }
 }
