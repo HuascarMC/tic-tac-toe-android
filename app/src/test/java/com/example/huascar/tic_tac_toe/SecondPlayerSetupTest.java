0f341fe53acc0e55959e95d2854f53052b526bf1
Huascar <huascarmc@hotmail.com>
2018-04-11 20:56:02 +0100

    added second player setup tests

--- app/src/test/java/com/example/huascar/tic_tac_toe/SecondPlayerSetupTest.java
+++ app/src/test/java/com/example/huascar/tic_tac_toe/SecondPlayerSetupTest.java
@@ -3,6 +3,9 @@ package com.example.huascar.tic_tac_toe;
 import com.example.huascar.tic_tac_toe.activities.SecondPlayerSetup;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SecondPlayerSetupTest {
 
@@ -12,4 +15,27 @@ public class SecondPlayerSetupTest {
     public void before() {
         secondPlayerSetup = new SecondPlayerSetup();
     }
+
+    @Test
+    public void testPlayerIsNull() {
+        assertEquals( secondPlayerSetup.getPlayer(), null );
+    }
+
+    @Test
+    public void testTokenIsNull() {
+        assertEquals( secondPlayerSetup.getToken(), null );
+    }
+
+    @Test
+    public void testSetPlayer() {
+        secondPlayerSetup.setPlayer("Human");
+        assertEquals( secondPlayerSetup.getPlayer(), "Human" );
+    }
+
+    @Test
+    public void testSetToken() {
+        secondPlayerSetup.setToken("O");
+        assertEquals( secondPlayerSetup.getToken(), "O" );
+    }
+
 }
