5d6a518f41ab8c84ba6fc2dc08fe0542dd01b1b3
Huascar <huascarmc@hotmail.com>
2018-04-11 20:58:20 +0100

    Added properties to SPS class and get player method test passed

--- app/src/main/java/com/example/huascar/tic_tac_toe/activities/SecondPlayerSetup.java
+++ app/src/main/java/com/example/huascar/tic_tac_toe/activities/SecondPlayerSetup.java
@@ -1,4 +1,35 @@
 package com.example.huascar.tic_tac_toe.activities;
 
-public class SecondPlayerSetup {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.example.huascar.tic_tac_toe.R;
+
+public class SecondPlayerSetup extends AppCompatActivity {
+
+    private String token;
+    private String player;
+    public Button humanButton;
+    private Button botButton;
+    private Button tokenXButton;
+    private Button tokenOButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_player_setup);
+
+        token = null;
+        player = null;
+
+        humanButton = findViewById(R.id.human_button);
+        botButton = findViewById(R.id.bot_button);
+        tokenXButton = findViewById(R.id.x_button);
+        tokenOButton = findViewById(R.id.o_button);
+    }
+
+    public String getPlayer() {
+        return player;
+    }
 }
