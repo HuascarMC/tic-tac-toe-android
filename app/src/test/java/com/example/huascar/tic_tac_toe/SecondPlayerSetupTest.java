6399407ba1f6cf02c1e0b3968f68ae20fa03c9f4
Huascar <huascarmc@hotmail.com>
2018-04-11 20:53:36 +0100

    created second player setup test file

---
+++ app/src/test/java/com/example/huascar/tic_tac_toe/SecondPlayerSetupTest.java
@@ -0,0 +1,13 @@
+package com.example.huascar.tic_tac_toe;
+
+import org.junit.Before;
+
+public class SecondPlayerSetupTest {
+
+    private SecondPlayerSetup secondPlayerSetup;
+
+    @Before
+    public void before() {
+        secondPlayerSetup = new SecondPlayerSetup();
+    }
+}
