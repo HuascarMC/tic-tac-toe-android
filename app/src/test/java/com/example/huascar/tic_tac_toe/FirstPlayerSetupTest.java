7030c81c1f1d5b3095f624a6fe26b131ec7c0c5c
Huascar <huascarmc@hotmail.com>
2018-04-11 16:22:35 +0100

    test player is null

--- app/src/test/java/com/example/huascar/tic_tac_toe/FirstPlayerSetupTest.java
+++ app/src/test/java/com/example/huascar/tic_tac_toe/FirstPlayerSetupTest.java
@@ -3,6 +3,9 @@ package com.example.huascar.tic_tac_toe;
 import com.example.huascar.tic_tac_toe.activities.FirstPlayerSetUp;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class FirstPlayerSetupTest {
 
@@ -13,5 +16,9 @@ public class FirstPlayerSetupTest {
         firstPlayerSetUp = new FirstPlayerSetUp();
     }
 
+    @Test
+    public void testPlayerIsNull() {
+        assertEquals( firstPlayerSetUp.getPlayer(), null );
+    }
 
 }
