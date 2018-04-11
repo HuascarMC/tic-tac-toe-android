002eaf7f9d471112dc910cc0d141b09923b080b2
Huascar <huascarmc@hotmail.com>
2018-04-11 20:59:05 +0100

    Set token test passed

--- app/src/main/java/com/example/huascar/tic_tac_toe/activities/SecondPlayerSetup.java
+++ app/src/main/java/com/example/huascar/tic_tac_toe/activities/SecondPlayerSetup.java
@@ -40,4 +40,8 @@ public class SecondPlayerSetup extends AppCompatActivity {
     public void setPlayer(String player) {
         this.player = player;
     }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
